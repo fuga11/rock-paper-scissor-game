92f74e5b1a513bfeb228ad0c6849e2f138764b6c
fuga11 <cikiblac35@gmail.com>
2025-03-14 20:32:48 +0300

    check if balance <= 1

--- scripts.js
+++ scripts.js
@@ -4,12 +4,17 @@ let score = JSON.parse(localStorage.getItem('score')) || {
   ties: 0
 };
 
-updateScoreElement();
-let PlayerBalance = 10;
-let isAutoPlaying = false;
-let intervalID;
-function UpdateBalance(){
-  document.querySelector('.balance').textContent = `You balance:${PlayerBalance}`;
+function IsZeroBalance(){
+  if (PlayerBalance > 1){
+    return true;
+  }
+  else{
+    setTimeout(()=>{
+      document.querySelector('.balance').textContent = `You balance ${PlayerBalance}`;
+    },3000);
+    document.querySelector('.balance').textContent = `Your balance is not enough for the game, please top it up.`;
+    return false;
+  }
 }
 function autoplay(){
   if (!isAutoPlaying){
@@ -18,75 +23,93 @@ function autoplay(){
       playGame(playerMove);
     }, 1000);
     isAutoPlaying = true;
-    UpdateBalanc();
   }else{
     clearInterval(intervalID);
     isAutoPlaying = false;
   }
 } 
+updateScoreElement();
+let PlayerBalance = 2;
+let isAutoPlaying = false;
+let intervalID;
+function UpdateBalance(){
+  document.querySelector('.balance').textContent = `You balance:${PlayerBalance}`;
+}
+function IsZeroBalance() {
+  if (PlayerBalance >= 1) {
+    return true;
+  } else {
+    document.querySelector('.balance').textContent = `Your balance is not enough for the game, please top it up.`;
+    setTimeout(() => {
+      document.querySelector('.balance').textContent = `Your balance: ${PlayerBalance}`;
+    }, 3000);
+    return false;
+  }
+}
 
 function playGame(playerMove) {
-  const computerMove = pickComputerMove();
-  let result = '';
+  if (IsZeroBalance()){
+    const computerMove = pickComputerMove();
+    let result = '';
+    if (playerMove === 'scissors') {
+      if (computerMove === 'rock') {
+        result = 'You lose.';
+        PlayerBalance -= 1;
+      } else if (computerMove === 'paper') {
+        result = 'You win.';
+        PlayerBalance += 1;
+      } else if (computerMove === 'scissors') {
+        result = 'Tie.';
+        PlayerBalance -= 0.25;
+      }
 
-  if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose.';
-      PlayerBalance -= 1;
-    } else if (computerMove === 'paper') {
-      result = 'You win.';
-      PlayerBalance += 1;
-    } else if (computerMove === 'scissors') {
-      result = 'Tie.';
-      PlayerBalance -= 0.25;
+    } else if (playerMove === 'paper') {
+      if (computerMove === 'rock') {
+        result = 'You win.';
+        PlayerBalance += 1;
+      } else if (computerMove === 'paper') {
+        result = 'Tie.';
+        PlayerBalance -= 0.25;
+      } else if (computerMove === 'scissors') {
+        result = 'You lose.';
+        PlayerBalance -= 1;
+      }
+      
+    } else if (playerMove === 'rock') {
+      if (computerMove === 'rock') {
+        result = 'Tie.';
+        PlayerBalance -= 0.25;
+      } else if (computerMove === 'paper') {
+        result = 'You lose.';
+        PlayerBalance -= 1;
+      } else if (computerMove === 'scissors') {
+        result = 'You win.';
+        PlayerBalance += 1;
+      }
     }
 
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win.';
-      PlayerBalance += 1;
-    } else if (computerMove === 'paper') {
-      result = 'Tie.';
-      PlayerBalance -= 0.25;
-    } else if (computerMove === 'scissors') {
-      result = 'You lose.';
-      PlayerBalance -= 1;
+    if (result === 'You win.') {
+      score.wins += 1;
+    } else if (result === 'You lose.') {
+      score.losses += 1;
+    } else if (result === 'Tie.') {
+      score.ties += 1;
     }
-    
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie.';
-      PlayerBalance -= 0.25;
-    } else if (computerMove === 'paper') {
-      result = 'You lose.';
-      PlayerBalance -= 1;
-    } else if (computerMove === 'scissors') {
-      result = 'You win.';
-      PlayerBalance += 1;
-    }
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = 
+    `You
+    <img class="move-ico" src="image/${playerMove}-emoji.png" alt="">
+    - 
+    <img class = "move-ico" src = "image/${computerMove}-emoji.png">
+    Computer`;
+    UpdateBalance();
   }
-
-  if (result === 'You win.') {
-    score.wins += 1;
-  } else if (result === 'You lose.') {
-    score.losses += 1;
-  } else if (result === 'Tie.') {
-    score.ties += 1;
-  }
-
-  localStorage.setItem('score', JSON.stringify(score));
-
-  updateScoreElement();
-
-  document.querySelector('.js-result').innerHTML = result;
-
-  document.querySelector('.js-moves').innerHTML = 
-  `You
-  <img class="move-ico" src="image/${playerMove}-emoji.png" alt="">
-  - 
-  <img class = "move-ico" src = "image/${computerMove}-emoji.png">
-  Computer`;
-  UpdateBalance();
 }
 
 function updateScoreElement() {
